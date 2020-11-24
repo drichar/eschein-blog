2d7717119a8680d2e05e08bfa551b3e0ab7f70b2
Doug Richar <drichar@gmail.com>
2020-11-24 03:13:21 -0500

    Show CoverImage bottom shadow

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,25 +1,11 @@
-import cn from 'classnames'
-import Link from 'next/link'
-
-export default function CoverImage({ title, url, slug }) {
-  const image = (
-    <img
-      src={url}
-      alt={`Cover Image for ${title}`}
-      className={cn('shadow-small', {
-        'hover:shadow-medium transition-shadow duration-200': slug,
-      })}
-    />
-  )
+export default function CoverImage({ title, url }) {
   return (
-    <div className="sm:mx-0">
-      {slug ? (
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
-          <a aria-label={title}>{image}</a>
-        </Link>
-      ) : (
-        image
-      )}
+    <div className="pb-4 sm:mx-0">
+      <img
+        src={url}
+        alt={`Cover Image for ${title}`}
+        className="shadow-small"
+      />
     </div>
   )
 }

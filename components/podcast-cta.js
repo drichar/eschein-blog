28ad6e888a1d40282028305ff1dfd2dc564f6f4b
Doug Richar <drichar@gmail.com>
2021-01-27 02:10:12 -0500

    Podcast CTA links to podcast page

--- components/podcast-cta.js
+++ components/podcast-cta.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 export default function PodcastCta() {
@@ -10,17 +11,21 @@ export default function PodcastCta() {
             <span className="block text-blue-600">And More.</span>
           </h2>
           <div className="my-4 prose prose-blue prose-lg xl:prose-xl text-gray-500">
-            <p>Evan is a family law and divorce attorney in New York City, and a partner and the Head of Litigation at <a href="https://www.berkbot.com/" target="_blank">Berkman Bottger Newman &amp; Schein</a>. He is the host of the podcast <a href="https://podcasts.apple.com/us/podcast/schein-on/id1541360422" target="_blank"><em>Schein On</em></a>, where he and his guests tackle tough legal, financial, and life issues, and talk about sports whenever they have an&nbsp;excuse.</p>
+            <p>Evan is a family law and divorce attorney in New York City, and a partner and the Head of Litigation at <a href="https://www.berkbot.com/" target="_blank">Berkman Bottger Newman &amp; Schein</a>. He is the host of the podcast <Link href="/schein-on"><a><em>Schein On</em></a></Link>, where he and his guests tackle tough legal, financial, and life issues, and talk about sports whenever they have an&nbsp;excuse.</p>
           </div>
         </div>
         <div className="mt-10 flex lg:mt-0 lg:ml-16 lg:flex-shrink-0">
-          <Image
-            className="rounded-lg shadow-lg m-auto md:m-0"
-            src="/images/schein-on.png"
-            alt="Schein On Podcast"
-            width={320}
-            height={320}
-          />
+          <Link href="/schein-on">
+            <a>
+              <Image
+                className="rounded-lg shadow-lg m-auto md:m-0"
+                src="/images/schein-on.png"
+                alt="Schein On Podcast"
+                width={320}
+                height={320}
+              />
+            </a>
+          </Link>
         </div>
       </div>
     </div>

900bcda8bf2afb95931c855b69f22b8b1a31bc0c
Doug Richar <drichar@gmail.com>
2021-03-17 14:39:56 -0400

    Podcast titles and descriptions

--- components/podcasts-content.js
+++ components/podcasts-content.js
@@ -8,15 +8,17 @@ export default function PodcastsContent({ allPodcasts }) {
       <div className="mt-8">
         <ul>
           {allPodcasts.map((podcast) => (
-            <li key={podcast.id} className="mb-16 shadow-xl">
+            <li key={podcast.id} className="mb-24 lg:mb-16">
               <iframe
                 src={`https://webplayer.adorilabs.com/${podcast.id}`}
-                className="h-96 w-full"
+                className="h-96 w-full mb-8 shadow-xl"
                 title={podcast.title}
                 frameBorder='0'
                 scrolling='no'
                 allow='autoplay'
               />
+              <h3 className="lg:hidden text-lg font-bold mb-4">{podcast.title}</h3>
+              <p className="lg:hidden">{podcast.excerpt}</p>
             </li>
           ))}
         </ul>

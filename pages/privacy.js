4cbdba2bfb3ca4f442f85f2cbb948dfbadb6bd7e
Doug Richar <drichar@gmail.com>
2021-03-24 07:16:54 -0400

    Privacy Policy

---
+++ pages/privacy.js
@@ -0,0 +1,40 @@
+import Container from '../components/container'
+import Layout from '../components/layout'
+import Head from 'next/head'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { getContentById } from '../lib/api'
+
+export default function PrivacyPolicy({ page }) {
+  const options = {
+    renderText: text => {
+      return text.split('\n').reduce((children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment];
+      }, []);
+    },
+  };
+
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>Privacy Policy | Evan Schein</title>
+        </Head>
+        <Container className="pb-16">
+          <div className="my-16 md:my-24 prose prose-blue prose-lg text-gray-500 text-sm md:text-base lg:text-lg mx-auto break-words">
+            {documentToReactComponents(page?.content?.json, options)}
+          </div>
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export async function getStaticProps() {
+  const data = await getContentById('privacy-policy')
+
+  return {
+    props: {
+      page: data ?? null
+    },
+  }
+}

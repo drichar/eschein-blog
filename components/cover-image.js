21e88dca6c5289370162d6b91634832e521d16c2
Doug Richar <drichar@gmail.com>
2021-04-02 16:37:26 -0400

    Center blog post images

--- components/cover-image.js
+++ components/cover-image.js
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function CoverImage({ title, coverImage }) {
   return (
-    <div className="pb-4 sm:mx-0 cover-image">
+    <div className="pb-4 sm:mx-0 cover-image text-center">
       <Image
         src={coverImage.url}
         alt={`Cover Image for ${title}`}
